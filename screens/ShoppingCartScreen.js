a16b7d077e35ac941632e1a01bb035bbdb17af80
00frans <everythingsdaijoufine@gmail.com>
2023-08-24 15:35:22 +0800

    added iconify

--- screens/ShoppingCartScreen.js
+++ screens/ShoppingCartScreen.js
@@ -1,10 +1,12 @@
-import { View, Text } from "react-native";
 import React from "react";
+import { View, Text } from "react-native";
+import { Iconify } from "react-native-iconify";
 
 const ShoppingCartScreen = () => {
   return (
     <View>
-      <Text className="px-2">ShoppingCartScreen</Text>
+      <Iconify icon="mdi:heart" size={24} color="#900" />
+      <Text>Hello, React Native Iconify!</Text>
     </View>
   );
 };
