cea9ee3fa25bfa4728fd12b9a32edb0e33cf52ea
00frans <everythingsdaijoufine@gmail.com>
2023-09-01 19:13:57 +0800

    Updated styling

--- components/styles.js
+++ components/styles.js
@@ -1,155 +1,202 @@
-import React, { useState } from "react";
-import { Iconify } from "react-native-iconify";
-import SwiperFlatList from "react-native-swiper-flatlist";
-import { useNavigation } from "@react-navigation/native";
-import {
-  View,
-  StyleSheet,
-  Text,
-  SafeAreaView,
-  Image,
-  ScrollView,
-  TouchableOpacity,
-  Dimensions,
-  Pressable,
-  FlatList,
-} from "react-native";
-import { TextInput } from "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
-import { filterData } from "../components/data";
+import styled from "styled-components";
+import { View, Text, Image, TextInput } from "react-native";
+import Constants from "expo-constants";
 
-const { width, height } = Dimensions.get("window");
+const StatusBarHeight = Constants.statusBarHeight;
 
-// Calculate the image dimensions based on screen size
-const imageWidth = width; // Adjust as needed
-const imageHeight = height * 0.18; // Adjust as needed
-const cardWidth = (width - 30) / 2;
-
-const HomeScreen = () => {
-  const navigation = useNavigation();
-  const [indexCheck, setIndexCheck] = useState("0");
-
-  const handleBranches = () => {
-    // Navigate to notification screen when TouchableOpacity of notification icon is pressed
-    navigation.navigate("BranchesScreen");
-  };
-
-  const renderPharmacy = ({ item }) => {
-    return (
-      <View className={`w-${cardWidth} ${tw("px-8 py-16")}`}>
-        <View className={`w-full ${tw("bg-white rounded-lg p-8 shadow-md")}`}>
-          <Image
-            style={`w-full h-90 ${tw("rounded-lg mt-5")}`}
-            source={item.image}
-          />
-          <Text
-            className={`text-center text-12 font-semibold text-3C3C3C ${tw(
-              "pt-2"
-            )}`}
-          >
-            {item.name}
-          </Text>
-          <TouchableOpacity onPress={handleBranches}>
-            <View
-              className={`w-full ${tw(
-                "bg-white flex-row items-center justify-center p-10 mt-14 border border-EC6F56 rounded-lg self-center"
-              )}`}
-            >
-              <Text
-                className={`text-EC6F56 text-11 font-semibold ${tw("mr-3")}`}
-              >
-                View Pharmacy
-              </Text>
-              <Iconify icon="ic:round-greater-than" size={15} color="#EC6F56" />
-            </View>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  };
-
-  return (
-    <SafeAreaView className="flex-1">
-      <ScrollView>
-        {/* Body */}
-        <View className={`px-15 pb-10 ${tw("flex-1")}`}>
-          <StatusBar backgroundColor="white" barStyle="light-content" />
-          <View className="mt-13 mb-5">
-            <Text className={`text-22 font-semibold ${tw("text-black")}`}>
-              E-
-              <Text className={`text-EC6F56 ${tw("")}`}> PharmaScripts</Text>
-            </Text>
-          </View>
-          <View
-            className={`flex-row items-center ${tw(
-              "bg-white shadow-md rounded-lg p-10 w-82% self-center"
-            )}`}
-          >
-            <Iconify
-              icon="circum:search"
-              size={22}
-              className={`text-22 ${tw("text-black mr-10")}`}
-            />
-            <TextInput placeholder="Search product" />
-          </View>
-          <TouchableOpacity>
-            <View className={`bg-black p-10 ml-15 ${tw("rounded-lg")}`}>
-              <Iconify icon="mi:filter" size={25} color="white" />
-            </View>
-          </TouchableOpacity>
-          <View
-            className={`justify-center items-center ${tw("mt-10 bg-gray-200")}`}
-          >
-            <SwiperFlatList
-              autoplay
-              autoplayDelay={2}
-              autoplayLoop
-              index={0}
-              showPagination
-              paginationDefaultColor="#ccc" // Customize pagination color
-              paginationActiveColor="#FF0000"
-              paginationStyle={`flex-row absolute bottom-24 self-center justify-center ${tw(
-                ""
-              )}`}
-              paginationStyleItem={`w-35 h-7 rounded-2 ml-5 mr-5 ${tw("")}`}
-              scrollEnabled={false} // Disable swiping
-              data={[{ image: require("../assets/img/ads.png") }]}
-              renderItem={({ item }) => (
-                <Image
-                  source={item.image}
-                  style={`w-${imageWidth} h-${imageHeight} ${tw("rounded-lg")}`}
-                />
-              )}
-            />
-            <Text
-              className={`text-12 text-black text-center text-xs p-5 ${tw("")}`}
-            >
-              Online pharmacy is the solution for a convenient way to buy
-              medicine!
-            </Text>
-          </View>
-          {/* PHARMACY SELECTION */}
-          <Text
-            className={`mt-10 pl-1 pb-2 text-16 font-semibold ${tw(
-              "text-3A3A3A"
-            )}`}
-          >
-            Pharmacy Selection
-          </Text>
-          <View className={`flex-1 ${tw("")}`}>
-            <FlatList
-              numColumns={2} // Display two items per row
-              scrollEnabled={false}
-              data={filterData}
-              keyExtractor={(item) => item.id.toString()}
-              extraData={indexCheck}
-              renderItem={renderPharmacy}
-            />
-          </View>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+// colors
+export const Colors = {
+  primary: "#ffffff",
+  secondary: "#E5E7EB",
+  tertiary: "#1F2937",
+  darkLight: "#9CA3AF",
+  light: "#F6F6F6",
+  brand: "#808080",
+  red: "#EB3F3F",
+  black: "#000000",
+  dark: "#273B4A",
+  gray: "#D9D9D9",
+  orange: "#EC6F56",
+  bodyGray: "#F5F5F5",
 };
 
-export default HomeScreen;
+const {
+  primary,
+  secondary,
+  tertiary,
+  darkLight,
+  light,
+  brand,
+  red,
+  black,
+  dark,
+  gray,
+  orange,
+  bodyGray,
+} = Colors;
+
+//register and login styles
+export const StyledContainer = styled.View`
+  flex: 1;
+  padding: ${StatusBarHeight + 5}px 25px 25px 25px;
+  background-color: ${primary};
+`;
+
+export const InnerContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+export const PageLogo = styled.Image`
+  width: 200px;
+  height: 150px;
+  align-self: center; /* Add this line to center the image horizontally */
+  margin-vertical: 20px; /* Add margin to adjust the vertical position if needed */
+`;
+export const SignupPageLogo = styled.Image`
+  width: 150px;
+  height: 100px;
+  align-self: center; /* Add this line to center the image horizontally */
+  margin-vertical: 20px; /* Add margin to adjust the vertical position if needed */
+`;
+export const SignupPageTitle = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${red};
+  padding: 8px;
+`;
+
+export const PageTitle = styled.Text`
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  color: ${red};
+  padding: 10px;
+`;
+
+export const Subtitle = styled.Text`
+  font-size: 18px;
+  text-align: center;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: ${dark};
+`;
+
+export const StyledFormArea = styled.View`
+  width: 100%;
+  padding: 0 16px;
+`;
+
+export const StyledTextInput = styled.TextInput`
+  background-color: ${primary};
+  padding-left: 30px;
+  border-radius: 18px;
+  font-size: 16px;
+  font-weight: 200;
+  height: 55px;
+  width: 100%;
+  color: ${dark};
+  border-width: 1px;
+  border-color: rgba(0, 0, 0, 0.2);
+  margin-left: 0;
+  text-align: left;
+  margin-vertical: 8px;
+`;
+
+export const RightIcon = styled.TouchableOpacity`
+  right: 20px;
+  top: 42px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  padding: 15px;
+  background-color: ${orange};
+  justify-content: center;
+  align-items: center;
+  border-radius: 26px;
+  margin-vertical: 5px;
+  height: 60px;
+  width: 100%; /* Use a percentage of the screen width */
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const ButtonText = styled.Text`
+  color: ${primary};
+  font-size: 20px;
+  font-weight: 600; /* Semi-bold font weight */
+  margin-top: -4px;
+`;
+
+export const MsgBox = styled.Text`
+  text-align: center;
+  font-size: 16px;
+  color: ${red};
+  padding: 8px;
+`;
+
+export const ExtraView = styled.View`
+  justify-content: center;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px;
+  margin-top: 30px;
+`;
+
+export const Extratext = styled.Text`
+  justify-content: center;
+  align-content: center;
+  color: ${black};
+  font-size: 17px;
+`;
+
+export const TextLink = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextLinkContent = styled.Text`
+  color: ${orange};
+  font-size: 17px;
+  font-weight: 700;
+`;
+
+//google
+export const GoogleButton = styled.TouchableOpacity`
+  padding: 15px;
+  background-color: ${gray};
+  border-radius: 23px;
+  margin-vertical: 5px;
+  flex-direction: row;
+  height: 60px;
+  justify-content: center; /* Center items horizontally */
+  align-items: center; /* Align items vertically */
+  width: 100%; /* Use a percentage of the screen width */
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const GoogleImage = styled.Image`
+  width: 10%;
+  height: 100%;
+  margin-right: 10px;
+`;
+
+export const GoogleText = styled.Text`
+  color: ${dark};
+  font-weight: 400; /*Regular font weight*/
+  font-size: 16px;
+`;
+export const Box = styled.Text`
+  text-align: center;
+  font-size: 16px;
+  color: ${dark};
+  padding: 8px;
+`;
+
+//register login styles end
